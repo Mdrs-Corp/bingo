96792d5cdea93a16ed6530b43537ed5cc28d918d
NICOLASkiwui <sauronk.travert@gmail.com>
2022-03-11 18:08:46 +0100

    m

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const grilles = fetch('grilles.json').then(response => response.json()).then(json => console.log(json));
+
 const grille = document.querySelector('#grille');
 const cases = 'abcdefghijklmnopqrstuvwxyz0123456789µ'.split('');
 
@@ -6,15 +8,16 @@ function gen(n) {
 	
 	const c = [...cases];
 	for (let i = 0; i < n*n; i++) {
-		const index = Math.floor(Math.random() * cases.length - i);
+		const index = Math.floor(Math.random() * (cases.length - i));
 		const texte = c[index];
 		c.splice(index, 1);
 		
 		const div = document.createElement('div');
 		div.className = 'case';
-		div.style = `width: calc(min(${50/n}vw, ${70/n}vh) - 4px); height: calc(min(${50/n}vw, ${70/n}vh) - 4px);`;
-		div.textContent = texte;
-		
+		div.style = `width: calc(min(${70/n}vw, ${70/n}vh) - 4px); height: calc(min(${70/n}vw, ${70/n}vh) - 4px);`;
+		const p = document.createElement('p');
+		p.textContent = texte;
+		div.appendChild(p);
 		grille.appendChild(div);
 	}
 }
