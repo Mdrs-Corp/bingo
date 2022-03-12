84085dbe9a946210219aa9ad0759cad0dbee731c
NICOLASkiwui <sauronk.travert@gmail.com>
2022-03-12 10:56:24 +0100

    d

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-const grilles = fetch('grilles.json').then(response => response.json()).then(json => console.log(json));
+//const grilles = fetch('grilles.json').then(response => response.json()).then(json => console.log(json));
+const grilles = JSON.parse(`
+{
+    "maths exp": [
+        "Daria mange",
+        "Théo fait un bruit sus",
+        "Antoine est prié de se taire",
+        "\\\"la pause monsieur ! la pause !!!\\\"",
+        "blague de Florian",
+        "Thomas fait tout autre chose",
+        "précision signée Rayan",
+        "Plee \\\"n'a rien compris\\\"",
+        "Olarribau vs l'ordi",
+        "démonstration super longue et sus",
+        "hilarité générale",
+        "\\\"Nico ? Nico !!\\\"",
+        "allusion de Larrib à la tablette",
+        "quelqu'un se lève sans raison",
+        "Olarribau fait ZUT!",
+        "de confusion Olarribau efface 3 fois ou plus un résultat"
+    ]
+}`);
 
 const grille = document.querySelector('#grille');
-const cases = 'abcdefghijklmnopqrstuvwxyz0123456789µ'.split('');
 
-function gen(n) {
+/**
+ * @param {number} n 
+ * @param {string[]} data 
+ */
+function gen(n, data) {
 	grille.innerHTML = '';
-	
-	const c = [...cases];
+	const c = [...data];
+
 	for (let i = 0; i < n*n; i++) {
-		const index = Math.floor(Math.random() * (cases.length - i));
+		const index = Math.floor(Math.random() * (data.length - i));
 		const texte = c[index];
 		c.splice(index, 1);
 		
@@ -22,4 +46,5 @@ function gen(n) {
 	}
 }
 
-document.querySelector('input').addEventListener('click', () => gen(2))
+document.querySelector('input').addEventListener('click', () => gen(3, grilles['maths exp']));
+gen(1, ['mdrs'])
